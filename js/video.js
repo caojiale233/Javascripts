43c5681f6b14ec76fc89ca5f7606c0b08202582b
caojiale233 <45928024+caojiale233@users.noreply.github.com>
2019-01-21 19:52:55 +0800

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,9 +1,8 @@
-var youku = '<span class="_icons">优</span>'
-
-var qq = '<span class="_icons">腾</span>'
-
-var all = '<span class="_icons">综</span>'
-
+var attr="^http[A-z]+://[A-z]*.*:{0}";
+if(location.href.match(attr+".iqiyi.com") || location.href.match(attr+".youku.com") || location.href.match(attr+".le.com") || location.href.match(attr+".letv.com") || location.href.match(attr+"v.qq.com") || location.href.match(attr+".tudou.com") || location.href.match(attr+".mgtv.com") || location.href.match(attr+"film.sohu.com") || location.href.match(attr+"tv.sohu.com") || location.href.match(attr+".acfun.cn") || location.href.match(attr+".bilibili.com") || location.href.match(attr+".pptv.com") || location.href.match(attr+"vip.1905.com") || location.href.match(attr+".yinyuetai.com") || location.href.match(attr+".fun.tv") || location.href.match(attr+".56.com") || location.href.match(attr+".wasu.cn")){
+var youku = '<span class="_icons you">优</span>'
+var qq = '<span class="_icons ten">腾</span>'
+var all = '<span class="_icons all">综</span>'
 var apis = [{
 name:all + "百域学院",url:"http://api.baiyug.cn/vip/index.php?url="
 },{
@@ -19,25 +18,18 @@ name:all + "ODFLV",url:"http://aikan-tv.com/?url="
 },{
 name:all + "无名小站",url:"http://www.wmxz.wang/video.php?url="
 } ];
-//create
+//create List
 function createSelect (apis) {
 	var myul = document.createElement("ul");
 	myul.id = "myul";
-	myul.setAttribute("style","display:none;background:#fff;box-shadow:0px 1px 10px rgba(0,0,0,0.3);margin:0;padding:0 4.2vw;position:fixed;bottom:33vh;right:12vw;z-index:99999;height:50vh;overflow:scroll;border-radius:1.26vw;");
 	for (var i = 0; i < apis.length; i ++) {
 		var myli = document.createElement("li");
 		var that=this;
-		myli.setAttribute("style","margin:0;padding:0;display:block;list-style:none;font-size:4.2vw;width:33.6vw;text-align:left;line-height:12.6vw;letter-spacing:0;border-bottom:1px solid #f0f0f0;position:relative;overflow:hidden;text-overflow:hidden;white-space:nowrap;");
+		myli.className="_list";
 		(function (num) {
 			myli.onclick = function () {
 				window.open(apis[num].url + location.href,'_blank');
 			};
-			myli.ontouchstart = function () {
-				this.style.cssText += "color:yellow;background:#373737;border-radius:1.26vw;";
-			}
-			myli.ontouchend = function () {
-				this.style.cssText += "color:black;background:transparent;border-radius:0;";
-			}
 		})(i);
 		myli.innerHTML = apis[i].name;
 		myul.appendChild(myli);
@@ -45,32 +37,25 @@ function createSelect (apis) {
 	document.body.appendChild(myul);
 }
 
-//
+//create switch
+var $switch=false;
 function createMenu(){
 	var $newStyle=document.createElement("style");
-	$newStyle.innerHTML="_icons{display:block;float:left;width:5vw;height:5vw;font-size:2.5vw;color:#fff;line-height:5vw;text-align:center;border-radius:100%;box-shadow:0px 0px 3px #a9a9a9;background:#0078FF;margin:3.78vw 2.1vw;}"
+	$newStyle.innerHTML="";
 	document.body.appendChild($newStyle);
 	var myBtn = document.createElement("div");
 	myBtn.id = "myBtn";
-	myBtn.innerHTML = "+";
-	myBtn.setAttribute("style","width:15vw;height:15vw;position:fixed;bottom:20vh;right:10vw;z-index:100000;border-radius:100%;text-align:center;line-height:15vw;box-shadow:0px 1px 10px rgba(0,0,0,0.3);font-size:5.5vw;background:#fff;");
+	myBtn.innerHTML = "<img class='_btnImg' src='https://img.icons8.com/ios-glyphs/90/multiply.png'>";
 	myBtn.onclick = function (){
-		var myul = document.getElementById("myul");
-		if(myul.style.display == "none"){
-			myul.style.display = "block";
-			this.style.transform="rotateZ(45deg)";
+		if($switch){
+			document.querySelector('._btnImg').style.transform="rotate(-45deg)";
+			document.querySelector('#myul').style.height="0vh";
 		}else{
-			myul.style.display = "none";
-			this.style.transform="rotateZ(0deg)";
+			document.querySelector('._btnImg').style.transform="rotate(90deg)";
+			document.querySelector('#myul').style.height="50vh";
+			
 		}
+		$switch=!$switch;
 	}
 	document.body.appendChild(myBtn);
-}
-/*document.addEventListener("DOMContentLoaded",function () {
-	createMenu();
-	createSelect(apis);
-});*/
-if(location.href.match(".iqiyi.com") || location.href.match(".youku.com") || location.href.match(".le.com") || location.href.match(".letv.com") || location.href.match("v.qq.com") || location.href.match(".tudou.com") || location.href.match(".mgtv.com") || location.href.match("film.sohu.com") || location.href.match("tv.sohu.com") || location.href.match(".acfun.cn") || location.href.match(".bilibili.com") || location.href.match(".pptv.com") || location.href.match("vip.1905.com") || location.href.match(".yinyuetai.com") || location.href.match(".fun.tv") || location.href.match(".56.com") || location.href.match(".wasu.cn")) {
-		createMenu();
-		createSelect(apis);
 }
