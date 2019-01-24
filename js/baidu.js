5e42c79d42c421e423a59dabbe6fb99448b5e31c
caojiale233 <45928024+caojiale233@users.noreply.github.com>
2019-01-24 19:39:42 +0800

    Update baidu.js

--- js/baidu.js
+++ js/baidu.js
@@ -1,7 +1,9 @@
 window.onload=function(){
   var $elements=document.querySelectorAll(".ec-tuiguang");
   for(var $i=0;$i<$elements.length;$++){
+    alert($i);
     if($elements[$i].innerHTML=="广告"){
+      alert($i+"/"+$elements.length);
       document.querySelector("#results").removeChild($elements[$i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode)
     }
   }
